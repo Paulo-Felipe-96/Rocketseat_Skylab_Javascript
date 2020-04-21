f7bed535d86c7d80d62969a024efb278e9f5f078
Paulo Martins <paulo.lip@hotmail.com>
2020-04-21 02:51:39 -0300

    added axios

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
-var minhaPromise = function () {
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/Paulo-Felipe-96');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-};
-
-minhaPromise()
+axios.get('https://api.github.com/users/Paulo-Felipe-96')
     .then(function (response) {
         console.log(response)
     })
